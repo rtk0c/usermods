acbe5c581e24eff0fdd8c68ff4da85c70314f012
rtk0c <mail.tianyig@gmail.com>
2025-01-18 23:18:54 -0800

    Replace open-in-new-tab from alt-click icon to alt-middleclick file name

--- github_open_raw.user.js
+++ github_open_raw.user.js
@@ -3,34 +3,34 @@
 // @namespace   https://github.com/rtk0c
 // @match       https://github.com/*
 // @grant       none
-// @version     1.0
+// @version     2.0
 // @author      rtk0c
 // @description Open files in raw.githubusercontent.com by holding alt
 // @run-at      document-idle
 // ==/UserScript==
 
-const targets = [
-    // We can't just use String.replace(), beacuse "blob" is a valid repo name
-    { cl: 'a.Link--primary', regex: /^(.+)\/(.+)\/blob\/(.+)/, method: url => { window.location.href = url } },
-    // FIXME why this locates no elements?
-    { cl: 'a.rgh-quick-file-edit', regex: /^(.+)\/(.+)\/edit\/(.+)/, method: window.open },
-]
+for (const n of document.querySelectorAll('a.Link--primary')) {
+    const matches = n.href.match(/^https?:\/\/github\.com\/(.+)\/(.+)\/blob\/(.+)/)
+    // Directories don't have raw resources
+    // they won't match beacuse URL will be /tree/ instead of /blob/
+    if (!matches)
+        continue
+        const [_, user, repo, path] = matches
+        const targetHref = `https://raw.githubusercontent.com/${user}/${repo}/refs/heads/${path}`
 
-for (const target of targets) {
-    for (const n of document.querySelectorAll(target.cl)) {
-        const matches = n.href.replace(/^https?:\/\/github\.com\//, '').match(target.regex)
-        if (!matches)
-            continue
+        // FIXME: this is not good for accesibility
+        n.title = 'alt-click to open the file in raw.githubusercontent.com'
 
-            // FIXME: this is not good for accesibility
-            n.title = 'alt-click to open the file in raw.githubusercontent.com'
-
-            n.addEventListener('click', e => {
-                if (e.altKey) {
-                    e.preventDefault()
-                    const [_, user, repo, path] = matches
-                    target.method(`https://raw.githubusercontent.com/${user}/${repo}/refs/heads/${path}`)
-                }
-            })
-    }
+        n.addEventListener('click', e => {
+            if (e.altKey) {
+                e.preventDefault()
+                window.location.href = targetHref
+            }
+        })
+        n.addEventListener('auxclick', e => {
+            if (e.altKey) {
+                e.preventDefault()
+                window.open(targetHref)
+            }
+        })
 }
